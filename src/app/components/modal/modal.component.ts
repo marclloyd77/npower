e98ff535064fa95a38fcd6e2dd6ed7b570614b59
Marc Lloyd <marc@marclloyd.co.uk>
2018-06-28 09:17:08 +0100

    Returning focus to modal open button after modal closes.

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -11,8 +11,10 @@ export class ModalComponent implements OnInit {
     showModal: boolean;
     private keyDownEvents;
     private clickEvents;
+    private closeButtonEvents;
     @Input() options: ModalOptions;
     @ViewChild( 'modalCloseButton' ) modalCloseButton: ElementRef;
+    @ViewChild( 'modalOpenButton' ) modalOpenButton: ElementRef;
     @ViewChild( 'overlay' ) overlay: ElementRef;
 
     constructor() {
@@ -20,7 +22,7 @@ export class ModalComponent implements OnInit {
 
     ngOnInit() {
         this.keyDownEvents = ( e ) => {
-            if ( e.code === 'Escape' || e.code === 'Enter' ) {
+            if ( e.code === 'Escape' ) {
                 this.closeModal();
             } else if ( e.code === 'Tab' || e.code === 'ShiftLeft' ) {
                 e.preventDefault();
@@ -32,6 +34,12 @@ export class ModalComponent implements OnInit {
 
         this.clickEvents = ( e ) => {
             if ( e.srcElement.className ) {
+                this.closeModal();
+            }
+        };
+
+        this.closeButtonEvents = ( e ) => {
+            if ( e.code === 'Enter' || e.code === 'Space' ) {
                 this.closeModal();
             }
         };
@@ -57,6 +65,7 @@ export class ModalComponent implements OnInit {
          * Add event listeners for focus trap and keyboard controls
          */
         window.addEventListener( 'keydown', this.keyDownEvents );
+        this.modalCloseButton.nativeElement.addEventListener( 'keydown', this.closeButtonEvents );
         this.overlay.nativeElement.addEventListener( 'click', this.clickEvents );
     }
 
@@ -72,6 +81,11 @@ export class ModalComponent implements OnInit {
          * Allow body scrolling
          */
         document.getElementsByTagName( 'body' )[ 0 ].removeAttribute( 'style' );
+
+        setTimeout( () => {
+            this.modalOpenButton.nativeElement.focus();
+        }, 0 );
+
 
         this.showModal = false;
     }
